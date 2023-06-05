9670902875aed270019b6247f9c8efb496df806a
Duwadi <kduwadi@nrel.gov>
2023-06-05 14:34:13 -0600

    updated setup.py to increase version to 1.0.0-alpha

--- setup.py
+++ setup.py
@@ -19,11 +19,12 @@ with open('requirements_dev.txt') as f:
 
 
 setup(name='erad',
-    version="0.1.0-alpha", # MAJOR.MINOR.PATCH (https://semver.org/)
-    description='Graph based scalable tool for computing equitable resilience metrics',
-    author='Aadil Latif, Kapil Duwadi, Sherin Ann Abraham, Kwami Sedzro, Jeremy Keen, Bryan Palmintier',
-    author_email='sherinann.abraham@nrel.gov, kwami.sedzro@nrel.gov, aadil.latif@nrel.gov, kapil.duwadi@nre.gov, jeremy.keen@nrel.gov, bryan.palmintier@nrel.gov',
-    url='https://github.nrel.gov/ERAD/erad',
+    version="1.0.0-alpha", # MAJOR.MINOR.PATCH (https://semver.org/)
+    description='Graph based scalable tool for computing equitable \
+        resilience metrics for distribution systems.',
+    author='Kapil Duwadi, Aadil Latif, Kwami Sedzro,  Sherin Ann Abraham, Bryan Palmintier',
+    author_email='kapil.duwadi@nre.gov, aadil.altif@nrel.gov, sherinann.abraham@nrel.gov, kwami.sedzro@nrel.gov, bryan.palmintier@nrel.gov',
+    url='https://github.com/nrel/erad',
     packages=find_packages("src"),
     install_requires=requirements,
     python_requires='>=3.8',
